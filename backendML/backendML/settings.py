29e704b5e83d553ab58242ed4722c0862fa711f1
clencyc <clency2023@gmail.com>
2024-10-10 00:25:06 +0300

    Configured databases

--- backendML/backendML/settings.py
+++ backendML/backendML/settings.py
@@ -80,7 +80,11 @@ WSGI_APPLICATION = 'backendML.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'fraud_db',
+        'USER': 'fraud_user',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
