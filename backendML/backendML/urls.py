e268a702dacd6827fb3c41cdb45e3263fb9b4601
clencyc <clency2023@gmail.com>
2024-10-29 22:59:01 +0300

    Added readme

--- backendML/backendML/urls.py
+++ backendML/backendML/urls.py
@@ -2,8 +2,6 @@ from django.urls import path, include
 from django.contrib import admin
 from rest_framework import routers
 from core import views as core_views
-from rest_framework.authtoken.views import obtain_auth_token
-
 # Import views 
 
 router = routers.DefaultRouter()
@@ -14,12 +12,15 @@ router.register(r'transactions', core_views.TransactionViewSet, basename='transa
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),  # Include the router URLs (if using viewsets)
-    
+    # creating the APIs
+    path('training/', views.TrainChurnModel.as_view(), name = 'model_training'),
+    path('prediction/', views.PredChurnModel.as_view(), name = 'model_prediction')
+
     path('api/users/', include('users.urls')),
     path('api/notifications/', include('notifications.urls')),
     path('api/reports/', include('reports.urls')),
     path('api/core', include('core.urls')),
-    
+
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # Add this line
 
 ]
