0d6a6d689e6e096b06b95ccba06cbbc9a2345b72
clencyc <clency2023@gmail.com>
2024-11-11 16:27:39 +0300

    deleted backend folder

--- backendML/users/urls.py
+++
@@ -1,8 +0,0 @@
-# users/urls.py
-from django.urls import path
-from .views import RegisterAPIView
-
-urlpatterns = [
-    path('register/', RegisterAPIView.as_view(), name='register_user'),
-    path('login/', LoginView.as_view())
-]
